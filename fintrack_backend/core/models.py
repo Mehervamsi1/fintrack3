5082f7eabf14993f0f6ceb46ac1e9b5de92ed370
Meher Vamsi Dontoju <75484855+Mehervamsi1@users.noreply.github.com>
2025-04-03 23:30:38 -0400

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch master
     Your branch is up to date with 'orgin/master'.
    
     Changes to be committed:
    	modified:   Fintrack PG Local.session.sql
    	new file:   fintrack_backend/core/__pycache__/__init__.cpython-39.pyc
    	new file:   fintrack_backend/core/__pycache__/admin.cpython-39.pyc
    	new file:   fintrack_backend/core/__pycache__/apps.cpython-39.pyc
    	new file:   fintrack_backend/core/__pycache__/models.cpython-39.pyc
    	modified:   fintrack_backend/core/admin.py
    	new file:   fintrack_backend/core/migrations/0001_initial.py
    	new file:   fintrack_backend/core/migrations/__pycache__/0001_initial.cpython-39.pyc
    	new file:   fintrack_backend/core/migrations/__pycache__/__init__.cpython-39.pyc
    	modified:   fintrack_backend/core/models.py
    	modified:   fintrack_backend/fintrack_backend/__pycache__/settings.cpython-39.pyc

--- fintrack_backend/core/models.py
+++ fintrack_backend/core/models.py
@@ -1,3 +1,30 @@
+# Create your models here.
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Expense(models.Model):
+    PAYMENT_METHODS = [
+        ('CASH', 'Cash'),
+        ('CARD', 'Credit/Debit Card'),
+        ('UPI', 'UPI'),
+        ('BANK', 'Bank Transfer'),
+        ('OTHER', 'Other'),
+    ]
+
+    purchase_date = models.DateField()
+    item = models.CharField(max_length=255)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    payment_method = models.CharField(max_length=10, choices=PAYMENT_METHODS)
+    receipt = models.FileField(upload_to='receipts/', blank=True, null=True)
+    place_of_purchase = models.CharField(max_length=255, blank=True)
+    notes = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"{self.item} - ${self.amount}"
